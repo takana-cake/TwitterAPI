9f69209ceb56266102c9b6cc504ebdd5abe6988f
takana-cake <msdf303@live.jp>
2019-12-10 13:26:45 +0900

    Create tokenview.php

---
+++ tokenview.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<META http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<TITLE></TITLE>
+</HEAD>
+<BODY>
+
+<?php
+	if(isset($_GET['oauth_token'])) { $oatoken = $_GET['oauth_token'];}
+	if(isset($_GET['oauth_verifier'])) { $oaveri = $_GET['oauth_verifier'];}
+	if (isset($oatoken) && isset($oaveri)) {
+		$command="python3 /var/www/cgi-bin/tokenview.py ".$oatoken." ".$oaveri; ?>
+		exec($command,$output);
+		echo $output[0];
+	} else {
+		$command="python3 /var/www/cgi-bin/tokenview.py";
+		exec($command,$output);
+		if(isset($output)) { print "<a href=$output[0]>link</a>"; }
+	}
+
+?>
+
+<hr>
+<a href="../../index.php">back</a>
+
+</BODY>
+</HTML>
